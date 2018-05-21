0b42e7f94eef1004829c10bb54e4f84944450171
Martin Carusso <martincarusso@gmail.com>
2018-05-21 19:07:15 -0300

    new test_animation done, using ax.plot instead of ax.scatter

--- test_animation_alt.py
+++ test_animation_alt.py
@@ -12,23 +12,23 @@ from iccpy.gadget import load_snapshot
 from iccpy.gadget.labels import cecilia_labels
 
 
-number_of_snapshots = 1   # actually includes snap_000
+number_of_snapshots = 200   # actually includes snap_000
 
 file = '../outputs/snap_{:03d}'
 
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig, (ax_1, ax_2, ax_3) = plt.subplots(1, 3)
-points_1 = ax_1.scatter([], [])
-points_2 = ax_2.scatter([], [])
-points_3 = ax_3.scatter([], [])
+points_1, = ax_1.plot([], [], 'bo')
+points_2, = ax_2.plot([], [], 'bo')
+points_3, = ax_3.plot([], [], 'bo')
 
 points = [points_1, points_2, points_3]
 
 # Function called to create the base frame upon which the animation takes place.
 def init():
 
-	points[0].set_offsets(np.array([], []))
+	points[0].set_data([], [])
 	#points[1].set_offsets()
 	#points[2].set_offsets()	
 
@@ -43,7 +43,7 @@ def animate(i):
 
 	x, y, z = np.split(gas_pos, 3, axis = 1)
 
-	points[0].set_offsets(gas_pos[:,:2])	
+	points[0].set_data(x, y)	
 	#points[1].set_offsets([x,z])
 	#points[2].set_offsets([y,z])
 	
@@ -55,7 +55,7 @@ anim = animation.FuncAnimation(fig, animate, init_func = init, frames = number_o
 #save the animation using ffmpeg
 dpi = 30
 writer = animation.writers['ffmpeg'](fps=30)
-anim.save('demo.mp4',writer=writer,dpi=dpi)
+anim.save('demo_alt.mp4',writer=writer,dpi=dpi)
 
 
 
