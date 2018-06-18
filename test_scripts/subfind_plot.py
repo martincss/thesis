1b7262b3dc0db049ecc85497c251bba873d127f3
Martín Carusso <martincarusso@gmail.com>
2018-06-18 00:07:47 -0300

    better modularized, condensed plotting into one function

--- test_scripts/subfind_plot.py
+++ test_scripts/subfind_plot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Test script to try out subfind and plotting
-for each subhalo
+for each subhalo.
 
 """
 
@@ -13,32 +13,48 @@ from iccpy.gadget.labels import cecilia_labels
 from iccpy.gadget.subfind import SubfindCatalogue
 from iccpy.utils import match
 
-
+# paths to the parent directory of the folders containing snapshots and subfind 
+# outputs must be supplied
 subfind_path = '../../2Mpc_LG'
 snap_num = 135
 snap_dir = '../../2Mpc_LG'
 
-
+# we create the Subfind object for that particular snapshot
 cat = SubfindCatalogue(subfind_path, snap_num)
 # Directory and snapnum are passed instead of filename, to open snapshot in multiple parts
-snap = load_snapshot(directory = snap_dir.format(snap_num), snapnum = snap_num, label_table = cecilia_labels)
+snap = load_snapshot(directory = snap_dir.format(snap_num), snapnum = snap_num, 
+	label_table = cecilia_labels)
 
 gas_ids = snap['ID  '][0]
 stars_ids = snap['ID  '][4]
-#gas_groups = cat.
 
-fig, ax = plt.subplots()
 
-for subhalo in cat.subhalo[:5]:
+def plotxy_subhalo(subhalo, num_species):
+	"""
+	Given a subhalo object, and species number (0 for gas, 4 for stars, etc.), 
+	plots the XY projection of the particles onto the axis object. 
+	"""
 
-	sub_ids = subhalo.ids
-	indexes = match(sub_ids, gas_ids)
+	# we first find the indices of the IDs array in which the subhalo IDs are
+	# located. We then filter out the -1s in the indexes array, since these are
+	# the subhalo IDs unmatched to the snap IDs for the particular species.
+	indexes = match(subhalo.ids, snap['ID  '][num_species])
 	indexes = indexes[indexes != -1]
 
-	subhalo_positions = snap['POS '][0][indexes]
+	subhalo_positions = snap['POS '][num_species][indexes]
 	x, y, z = np.split(subhalo_positions, 3, axis = 1)
 
 	ax.scatter(x,y, s = 0.01)
+
+
+fig, ax = plt.subplots()
+
+# Define number of subhaloes to plot (if -1, plot all of them)
+number_of_subhaloes = 20
+
+for subhalo in cat.subhalo[:number_of_subhaloes]:
+
+	plotxy_subhalo(subhalo, 4)
 
 fig.show()
 
