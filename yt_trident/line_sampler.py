201c88a7af47fd595e19bf8a8fed10975a9ffe09
Martín Carusso <martincarusso@gmail.com>
2018-10-12 11:35:48 -0300

    ready to sample, but runs out of memory while doing it :(

--- yt_trident/line_sampler.py
+++ yt_trident/line_sampler.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from numpy import pi as pi
 import matplotlib.pyplot as plt
+plt.ion()
 import yt
 import trident
 
@@ -32,7 +34,7 @@ mw_center = subhalo_center(subfind_path=subfind_path, snap_num=snap_num,
 
 def make_ray_from_mw(spherical_coords, ray_filename):
 
-    ray_end = ray_end_from_sph(mw_center, spherical_coords)
+#    ray_end = ray_end_from_sph(mw_center, spherical_coords)
 
     print('el ray_end antes es ', ray_end)
     # for some reason, make_simple_ray overwrites start_position and end_position
@@ -46,22 +48,33 @@ def make_ray_from_mw(spherical_coords, ray_filename):
                                   lines=line_list,
                                   ftype='Gas')
 
-    print('los mw_center y ray_end después son ', mw_center, ray_end)
+#    print('los mw_center y ray_end después son ', mw_center, ray_end)
 
 
     return ray
 
 def make_ray_sample(r_interval, theta_interval, phi_interval):
 
-    for r, theta, phi in zip(r_interval, theta_interval, phi_interval):
+    for r in r_array:
 
-        ray_filename = './rays_2Mpc_LG/ray_{:.0f}_{:.0f}_{:.0f}.h5'.format(r, theta, phi)
+        for theta in theta_array:
 
-        make_ray_from_mw(*(r, theta, phi), ray_filename=ray_filename)
+            for phi in phi_array:
+
+                print('\n NOW SAMPLING r = {}, theta = {}, phi = {} ~~~~~~~~~~~~~~~~~~~ \n'.format(r, theta, phi))
+
+                ray_filename = './rays_2Mpc_LG/ray_{:.0f}_{:.1f}_{:.1f}.h5'.format(r, theta, phi)
+                ray_from_mw = make_ray_from_mw((r, theta, phi), ray_filename=ray_filename)
 
 
+# ~~~~~~~~~~~~~~~~ RAY SAMPLING ~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+r_array = np.array([1000])
+theta_array = np.linspace(0, pi, 10)
+phi_array = np.linspace(0, 2*pi, 10)
 
+make_ray_sample(r_array, theta_array, phi_array)
 
 
 #ray_test = make_ray_from_mw((1000, 0, 0), './rays_2Mpc_LG/ray_test.h5')
