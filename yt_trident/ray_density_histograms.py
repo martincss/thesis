0a283e41d6b91daca617067cb102d15e33f1f5bd
Martín Carusso <martincarusso@gmail.com>
2018-10-13 20:22:15 -0300

    cleaning up ray datasets folder

--- yt_trident/ray_density_histograms.py
+++ yt_trident/ray_density_histograms.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 plt.ion()
 import yt
 from tools import ray_mean_density
-
+import pandas as pd
 
 theta_array = np.linspace(0, pi, 10)
 phi_array = np.linspace(0, 2*pi, 10)
@@ -21,7 +21,7 @@ def get_mean_densities():
 
     mean_densities = []
 
-    for handle in glob.glob('./rays_2Mpc_LG/ray_1000*'):
+    for handle in glob.glob('./rays_2Mpc_LG/ray_1000_*_*'):
 
         ray = yt.load(handle)
 
@@ -46,6 +46,8 @@ def get_single_densities():
 
 mean_densities = get_mean_densities()
 singular_densities = get_single_densities()
+
+
 
 plt.figure()
 plt.hist(mean_densities, bins=20, color = 'red', edgecolor='black', linewidth=1.2)
