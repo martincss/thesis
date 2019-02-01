ad333c5a259a9fa029ce330ef20402285fd17bf2
Martin Carusso <martincarusso@gmail.com>
2019-01-31 23:52:29 -0300

    sampler parallel now ready to sample to m31 and away in distance

--- yt_trident/sampler_parallel.py
+++ yt_trident/sampler_parallel.py
@@ -1,13 +1,13 @@
+from numpy import pi
 import glob
 from absorber_file_generator import generate_one_to_map
 from multiprocessing import Pool, cpu_count
 from line_sampler_to_mw import sphere_uniform_grid, sample_one_to_map
+from tools import usable_cores
 #from line_sampler_from_any import sample_one_to_map_from_any
 
-if cpu_count() == 12:
-     number_of_cores = 5
-else:
-     number_of_cores = 2
+
+number_of_cores = usable_cores()
 pool = Pool(number_of_cores)
 
 #rays_directory = './rays_2Mpc_LG_to_mw_2000_wrt_mwcenter/'
@@ -33,12 +33,33 @@ def make_ray_sample_uniform(r_interval, number_of_sightlines, pool):
                 i, (theta, phi) in enumerate(zip(theta_interval, phi_interval))]
         pool.map(sample_one_to_map, tasks)
 
-def make_sample_from_any():
-    pass
+
+def sample_m31_and_away(r_interval, rays_directory, pool):
+    """
+    Samples rays on fixed directions to m31 and away from m31, varying distance
+    to endpoints.
+    """
+
+    theta_m31 = 2*pi/9
+    phi_m31 = 6*(2*pi)/9
+
+    theta_away = 3*pi/9
+    phi_away = 2*(2*pi)/9
+
+    thetas = [theta_m31, theta_away]
+    phis = [phi_m31, phi_away]
+
+    number_of_sightlines = 2*len(r_interval)//2
+
+    tasks = [(i, number_of_sightlines, r, theta, phi, rays_directory) for i, r \
+             in enumerate(r_interval) for (theta, phi) in zip(thetas, phis)]
+
+    pool.map(sample_one_to_map, tasks)
 
 
 
 if __name__ == '__main__':
     #generate_absorbers_sample(rays_directory, absorbers_directory, pool)
 
-    make_ray_sample_uniform([2000], 10, pool)
+    #make_ray_sample_uniform([2000], 10, pool)
+    sample_m31_and_away([10, 23], rays_directory, pool)
