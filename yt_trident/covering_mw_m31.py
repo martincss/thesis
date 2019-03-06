34d766c2e9c2385d7c7c5de4197255d8a4ef1636
Martin Carusso <martincarusso@gmail.com>
2019-03-05 22:35:28 -0300

    covering fraction comparision btw mw & m31 script

---
+++ yt_trident/covering_mw_m31.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+plt.ion()
+# plt.rc('text', usetex=True)
+# plt.rc('font', family='serif')
+from tools import HUBBLE_2Mpc_LG
+from profiles_mw import covering_fractions_2k
+from profiles_m31 import covering_fractions_m31
+
+abs_lines = ['Si III 1206', 'Si II 1193', 'C II 1335', 'C IV 1548']
+
+R_BIN_LENGTH = 3 # kpc/h
+
+thresh_1 = 1e10
+thresh_2 = 1e13
+
+def plot_covfs(r, covfs):
+
+    mw1, mw2, m311, m312 = covfs
+
+    fig, axarr = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
+
+    # this hurt me more than it did to you
+    for line, covf in mw1.items():
+        axarr[0,0].plot(r[:-1], covf, label = line)
+
+    for line, covf in mw2.items():
+        axarr[1,0].plot(r[:-1], covf, label = line)
+
+    for line, covf in m311.items():
+        axarr[0,1].plot(r[:-1], covf, label = line)
+
+    for line, covf in m312.items():
+        axarr[1,1].plot(r[:-1], covf, label = line)
+
+    axarr[1,0].set_xlabel('Distance [kpc]', fontsize = 15)
+    axarr[1,1].set_xlabel('Distance [kpc]', fontsize = 15)
+    axarr[0,0].set_ylabel('$f$', fontsize = 15)
+    axarr[0,0].set_title('MW', fontsize = 15)
+    axarr[0,1].set_title('M31', fontsize = 15)
+    axarr[1,0].set_ylabel('$f$', fontsize = 15)
+
+    for ax in axarr.flatten():
+        ax.grid()
+        ax.legend()
+
+    fig.suptitle('Covering fraction by distance', fontsize = 20)
+    fig.subplots_adjust(wspace=0, hspace=0)
+
+    return fig, axarr
+
+
+if __name__ == '__main__':
+
+    print('next covering fractions > {}'.format(thresh_1))
+    r, covfs_mw_first = covering_fractions_2k(abs_lines, thresh_1, R_BIN_LENGTH)
+    r, covfs_m31_first = covering_fractions_m31(abs_lines, thresh_1,R_BIN_LENGTH)
+
+    print('next covering fractions > {}'.format(thresh_2))
+    r, covfs_mw_second = covering_fractions_2k(abs_lines, thresh_2, R_BIN_LENGTH)
+    r, covfs_m31_second = covering_fractions_m31(abs_lines, thresh_2,R_BIN_LENGTH)
+
+    r /= HUBBLE_2Mpc_LG
+
+    fig, axarr = plot_covfs(r,[covfs_mw_first, covfs_mw_second,
+                               covfs_m31_first, covfs_m31_second])
